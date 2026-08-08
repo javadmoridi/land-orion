be8f6d801e011ff964777eb2eba32be6fb3e5e9b
javadmoridi <javadmoridi1999@gmail.com>
2026-08-08 19:38:03 +0330

    remove wallet connect startup

--- src/App.tsx
+++ src/App.tsx
@@ -1,82 +1,11 @@
-import { useEffect, useMemo } from 'react';
-import { TonConnectUIProvider, useTonWallet } from '@tonconnect/ui-react';
-
-import { WalletConnectionScreen } from './wallet/WalletConnectionScreen';
 import { GameWorld } from './game/GameWorld';
-import { useGameStore } from './game/useGameStore';
-import { getManifestUrl } from './wallet/walletService';
-
-
-function GameRouter() {
-  const {
-    isConnected,
-    playerProfile,
-    connectionStatus,
-    error,
-    disconnectWallet,
-  } = useGameStore();
-
-  const wallet = useTonWallet();
-
-
-  useEffect(() => {
-    if (!wallet && isConnected) {
-      disconnectWallet();
-    }
-  }, [wallet, isConnected, disconnectWallet]);
-
-
-  if (connectionStatus === 'connecting') {
-    return <div>Connecting wallet...</div>;
-  }
-
-
-  if (error) {
-    return (
-      <div>
-        <h3>Game Error</h3>
-        <p>{error}</p>
-
-        <button onClick={disconnectWallet}>
-          Disconnect
-        </button>
-      </div>
-    );
-  }
-
-
-  if (!isConnected || !playerProfile) {
-    return <WalletConnectionScreen />;
-  }
-
-
-  return <GameWorld />;
-}
-
-
 
 function App() {
-
-  const manifestUrl = useMemo(() => {
-    try {
-      return getManifestUrl();
-    } catch {
-      return '/tonconnect-manifest.json';
-    }
-  }, []);
-
-
   return (
-    <TonConnectUIProvider
-      manifestUrl={manifestUrl}
-      actionsConfiguration={{
-        twaReturnUrl: window.location.origin,
-      }}
-    >
-      <GameRouter />
-    </TonConnectUIProvider>
+    <div style={{ minHeight: '100vh' }}>
+      <GameWorld />
+    </div>
   );
 }
-
 
 export default App;
